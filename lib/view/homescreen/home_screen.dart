b163dd24da402b524e0af1c7f756fae1f052c42d
Sharon Rodrigues <sr979151@gmail.com>
2023-10-06 11:50:38 +0530

    updated

--- lib/view/homescreen/home_screen.dart
+++ lib/view/homescreen/home_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:todoapp/utils/colors.dart';
 import 'package:todoapp/utils/contants.dart';
-import 'package:todoapp/view/homescreen/widgets/bottom_sheet.dart';
 import 'package:todoapp/view/notescreen/note_screen.dart';
 
 List<Map<String, String>> tiledata = [];
@@ -120,12 +119,96 @@ showBottomSheet(BuildContext context) {
   TextEditingController titlleController = TextEditingController();
   TextEditingController descriptioncontroller = TextEditingController();
   TextEditingController dateController = TextEditingController();
+
   return showModalBottomSheet(
       context: context,
       builder: (context) {
-        return BottomSheetWidget(
-            titlleController: titlleController,
-            descriptioncontroller: descriptioncontroller,
-            dateController: dateController);
+        return StatefulBuilder(builder: (context, inSetState) {
+          late DateTime selectedDate;
+          bool isSelected = false;
+          return Container(
+            decoration: BoxDecoration(
+                color: ColorContants.bottomSheetColor,
+                borderRadius: BorderRadius.only(
+                    topRight: Radius.circular(30),
+                    topLeft: Radius.circular(30))),
+            child: Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Column(
+                children: [
+                  kheight20,
+                  TextFormField(
+                    controller: titlleController,
+                    decoration: InputDecoration(
+                        hintText: "Title",
+                        fillColor: Colors.white,
+                        filled: true,
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(15))),
+                  ),
+                  kheight10,
+                  TextFormField(
+                    controller: descriptioncontroller,
+                    maxLines: 5,
+                    decoration: InputDecoration(
+                        hintText: "Description",
+                        fillColor: Colors.white,
+                        filled: true,
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(15))),
+                  ),
+                  kheight10,
+                  TextFormField(
+                    controller: dateController,
+                    readOnly: true,
+                    decoration: InputDecoration(
+                        prefixIcon: InkWell(
+                            onTap: () async {
+                              DateTime? pickedDate = await showDatePicker(
+                                  context: context,
+                                  initialDate: isSelected
+                                      ? selectedDate
+                                      : DateTime.now(),
+                                  firstDate: DateTime.now(),
+                                  lastDate:
+                                      DateTime.now().add(Duration(days: 120)));
+
+                              if (pickedDate != null) {
+                                selectedDate = pickedDate;
+                                isSelected = true;
+                                final year = pickedDate.year;
+                                final month = pickedDate.month;
+                                final day = pickedDate.day;
+                                dateController.text = "$year-$month-$day";
+                              }
+                            },
+                            child: Icon(Icons.calendar_month)),
+                        hintText: "date",
+                        fillColor: Colors.white,
+                        filled: true,
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(15))),
+                  ),
+                  kheight20,
+                  ElevatedButton(
+                    onPressed: () {
+                      final Map<String, String> map = {};
+
+                      map["title"] = titlleController.text;
+                      map["desc"] = descriptioncontroller.text;
+                      map["date"] = dateController.text;
+                      tiledata.add(map);
+
+                      Navigator.pop(context);
+                    },
+                    child: Text("Add"),
+                    style: ButtonStyle(
+                        backgroundColor: MaterialStatePropertyAll(Colors.blue)),
+                  )
+                ],
+              ),
+            ),
+          );
+        });
       });
 }
