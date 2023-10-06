df24eb64d54fad358abbb214f58cfea063b9d413
Sharon Rodrigues <sr979151@gmail.com>
2023-10-06 14:51:37 +0530

    data model

---
+++ lib/model/data_model.dart
@@ -0,0 +1,8 @@
+class DataModel {
+  final String title;
+  final String description;
+  final String date;
+
+  DataModel(
+      {required this.date, required this.description, required this.title});
+}
